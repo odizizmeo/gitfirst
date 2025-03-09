7a77f285ffede8bc89207a23b870c48c9377c8c9
odizizmeo <saiandbas20@gmail.com>
2025-03-09 19:09:51 +0700

    fix sort bug

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ let numbe = [];
 for (let i=0;i<3;i++){
     let number = parseFloat(prompt(`Enter ${i + 1}`));
     numbe.push(number);   
-    numbe.sort();         
+    numbe.sort((a, b) => a - b);        
 }
 document.getElementById("number").innerHTML = numbe;   
